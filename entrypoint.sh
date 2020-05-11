f55ab3b75f78845f666c26bed8faa829d19aa8fd
mattmain <mainx016@gmail.com>
2020-05-11 14:23:36 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,13 @@
 #!/bin/sh -l
 
-#$1 Repository
-#$2 SFDX Auth URL
-#$3 Github Token
+#$1 SFDX Auth URL
 
-echo "Hello World"
-
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-#nvm install node
+#Check Node and NPM versions
 node -v
 npm -v
 
-#Install Salesforce CLI 
-#wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz
-#mkdir sfdx-cli
-#tar xJf sfdx-linux-amd64.tar.xz -C sfdx-cli --strip-components 1
-#./sfdx-cli/install
-#sfdx -v
-#echo salesforce cli install completed.
-
 #Populate Auth, Passed in as first variable
-echo $2 > ./SFDX_URL.txt
-echo '$2'
-echo $2
-echo cat
-cat ./SFDX_URL.txt
-#cat SFDX_URL.txt
-#echo 'file list'
-ls
-
+echo $1 > ./SFDX_URL.txt
 
 #Auth Org and Retreive Source
 sfdx -v
@@ -70,7 +49,7 @@ sfdx force:source:retrieve -u salesforce --verbose -m  \
 #  WorkflowAlert
   "
   
-  ls 
+ls 
   
-  #Remove Auth File
-  rm -f ./SFDX_URL.txt
+#Remove Auth File
+rm -f ./SFDX_URL.txt
